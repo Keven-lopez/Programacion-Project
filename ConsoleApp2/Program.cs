abc9e01ad6c4431ada7b1e5ac53fe722eb1efef0
Keven <klopezp40@miumg.edu.gt>
2024-02-14 22:01:46 -0600

    numero a decimal

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1;
+decimal num2;
+Console.WriteLine("Ingrese un numero");
+num1 = int.Parse(Console.ReadLine());
+num2 = num1;
+Console.WriteLine("")
